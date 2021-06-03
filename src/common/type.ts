f27027a50838b9f799921840ceddf3dcb4da4c33
Jose Salas <rigojunnior@gmail.com>
2021-06-03 08:23:05 -0400

    WIP adding comments

--- src/common/type.ts
+++ src/common/type.ts
@@ -1,10 +1,19 @@
 // ENTITIES
 
+export interface IComments {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
 export interface IPost {
   userId: number;
   id: number;
   title: string;
   body: string;
+  comments: IComments[]
 }
 
 // FATHERS
